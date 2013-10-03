2e8a45f1a31c9939241ffbae59fad1c2bfd8c7be
Sylvain Gizard <sylvain.gizard@gmail.com>
2013-10-03 18:57:40 +0200

    Correct handling of binary data

--- client.js
+++ client.js
@@ -2,8 +2,8 @@ SmartFile.upload = function (file, path, callback) {
     var fileReader = new FileReader();
 
     fileReader.onload = function(e) {
-        Meteor.call('sm.upload', file.name, path, e.target.result, callback);
+        Meteor.call('sm.upload', file.name, path, new Uint8Array(e.target.result), callback);
     };
 
-    fileReader.readAsBinaryString(file);
+    fileReader.readAsArrayBuffer(file);
 };
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var FormData = Npm.require('form-data');
+var formDataSubmitSync = Meteor._wrapAsync(FormData.prototype.submit);
 
 var SF_API_ENDPOINT = "app.smartfile.com";
+var SF_API_PATH = "/api/2/";
 
 SmartFile.setApiCredentials = function (key, password) {
     SmartFile.apiKey = key;
@@ -14,22 +16,36 @@ SmartFile.setBasePath = function (path) {
 SmartFile.postDataToPath = function (fileName, path, data) {
     var form = new FormData();
 
-    form.append('file', data, {
+    form.append('file', new Buffer(data), {
         filename: fileName
     });
 
-    form.submit({
+    var res = formDataSubmitSync.call(form, {
         protocol: "https:",
         host: SF_API_ENDPOINT,
-        path: "/api/2/path/data/" + SmartFile.basePath + path,
+        path: SF_API_PATH + "path/data/" + SmartFile.basePath + path,
         auth: SmartFile.apiKey + ":" + SmartFile.apiPassword
-    }, function(err, res) {
-        console.log(res.statusCode);
     });
+
+    return res.statusCode == 200;
 };
 
 Meteor.methods({
     "sm.upload": function (fileName, path, data) {
-        SmartFile.postDataToPath(fileName, path, data);
+        try {
+            var success = SmartFile.postDataToPath(fileName, path, data);
+
+            if (!success) {
+                throw new Error();
+            }
+
+            var sfPath = SF_API_ENDPOINT + SF_API_PATH + "path/data/" + SmartFile.basePath + "/" + fileName;
+            console.log("Stored file in SmartFile to path: " + sfPath);
+
+            return sfPath;
+        } catch (e) {
+            console.log(e);
+            throw new Meteor.Error(500, "SmartFile API error");
+        }
     }
 });
